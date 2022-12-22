bcdea2d3c7bd08bf540a3612c080c2ae3034ba00
oloruntoba oloyede <tobility83@gmail.com>
2022-12-21 17:43:56 -0700

    updated valued in aiye

--- aiye.sh
+++ aiye.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 echo gods saves 
-echo life is goog
+echo life is good.
+echo my heart is pure, and i have not learn any message from life.
+echo i have fought almost everyone, including myself
+
